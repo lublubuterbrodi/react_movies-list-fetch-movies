19cb8d835ab36126d14b05092d87b82ab4a04499
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-04-08 17:13:48 +0300

    add task solution

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,11 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=b066b974';
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  return fetch(`${API_URL}&t=${query}`)
+  return fetch(`${API_URL}&t=${encodeURIComponent(query)}`)
     .then(res => res.json())
     .catch(() => ({
       Response: 'False',
